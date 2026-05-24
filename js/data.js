98be3eb7fbd40e3ca4e0c9532a0b3dbfff7d37c2
Mortadha Ghaliounji <51703175+mortadhaghaliounji@users.noreply.github.com>
2026-05-24 21:52:12 +0100

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,41 @@
+const ASSETS_DATA = {
+    "FR": {
+        flag: "fr", 
+        files: ["af.svg", "upr.png", "r!.png", "dlf.svg", "il.png", "rn.png", "udr.svg", "rpf.svg", "ump.png", "lr.png", "horizons.png", "pr.png", "re.png", "modem.png", "pp.png", "ps.png", "sfio.svg", "gens.png", "apres.png", "lfi.png", "pcf.png", "eelv.png", "nfp.png", "nupes.png", "cgt.png"]
+    },
+    "UK": {
+        flag: "gb",
+        files: [ "ukip.png", "adv.jpg", "rest.png", "ref.png", "cons.png", "lab.png", "liberal.png", "libdem.png", "green.png", "yourparty.svg", "snp.png", "sf.png", "dup.png", "cd.png"]
+    },
+    "TN": {
+        flag: "tn",
+        files: [
+        "true25july.png", "ptr.png",
+        "psd.png", "pup.png",
+        "el-amen.png", "fsn.png",
+        "mup.png", "haq.png",
+        "ptv.png", "hizbelhiraq.png",
+        "mjt.png", "mds.png",
+        "ppt.png", "mta.png",
+        "upr.png", "nidaatounes.png",
+        "wafa.png", "paritsocalliberal.png",
+        "ppds.png", "aljoumhouri.png",
+        "mrt.png", "al-massar.png",
+        "pvp.png", "baath.png",
+        "thirdrepublic.png", "echaab.png",
+        "tnp.png", "ennahdha.png",
+        "upl.png", "errahmah.png",
+        "watad.png", "ettakatol.png",
+        "qalbtounes.png", "frontpopulaire.png",
+        "tahyatounes.png", "25july.png",
+        "albadil.png", "alkarama.png",
+        "azimoun.png", "attayar.png",
+        "cn.png", "alamal.png",
+        "pdl.png", "al-qotb.png",
+        "plt.png", "afektounes.png",
+        "popularcurrent.png",
+        "ps.png",
+        "pt.png",
+        "ptpd.png"
+        ]    }
+};
---
+++ js/i18n.js
@@ -0,0 +1,18 @@
+const translations = {
+    "fr": {
+        "t-chooseLogo": "Choisissez un logo",
+        "t-instructions": "Cliquez pour l'ajouter à votre chronologie.",
+        "t-yourJourney": "Votre Parcours Politique",
+        "t-editTip": "Astuce : Cliquez sur les dates dans le graphique pour les modifier !",
+        "t-clear": "Tout effacer",
+        "default-date": "Date de début\nDate de fin"
+    },
+    "en": {
+        "t-chooseLogo": "Choose a logo",
+        "t-instructions": "Click to add it to your timeline.",
+        "t-yourJourney": "Your Political Journey",
+        "t-editTip": "Tip: Click directly on the dates in the graph to edit them!",
+        "t-clear": "Clear all",
+        "default-date": "Start Date\nEnd Date"
+    }
+};
